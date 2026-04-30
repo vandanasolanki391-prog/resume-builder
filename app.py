a510ba0e15400178d01fa215cc6ed01785dc7684
vandanasolanki391-prog <vandanasolanki391@gmail.com>
2026-04-30 17:53:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,27 @@ def create():
 @app.route('/preview', methods=['POST'])
 def preview():
     data = request.form
-    return render_template('preview.html', data=data)
+
+    education = zip(
+        request.form.getlist('degree'),
+        request.form.getlist('institute'),
+        request.form.getlist('year'),
+        request.form.getlist('percentage')
+    )
+
+    experience = zip(
+        request.form.getlist('company'),
+        request.form.getlist('role'),
+        request.form.getlist('duration'),
+        request.form.getlist('description')
+    )
+
+    return render_template(
+        'preview.html',
+        data=data,
+        education=education,
+        experience=experience
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
