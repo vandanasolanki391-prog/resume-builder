d9510fada0d658ee2af9e51438747b82149f6434
vandanasolanki391-prog <vandanasolanki391@gmail.com>
2026-05-25 14:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
@@ -12,7 +17,25 @@ def create():
 
 @app.route('/preview', methods=['POST'])
 def preview():
-    data = request.form
+
+    data = request.form.to_dict()
+
+    # PHOTO UPLOAD
+    photo = request.files.get('photo')
+
+    if photo and photo.filename != '':
+        filename = secure_filename(photo.filename)
+
+        # static folder create if not exists
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+
+        photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        photo.save(photo_path)
+
+        data['photo'] = filename
+    else:
+        data['photo'] = None
 
     education = zip(
         request.form.getlist('degree'),
